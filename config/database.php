4cdd23c89daef8dcd6778d5995753fd23daa6cf7
sirabdull <sirabdull70@gmail.com>
2024-06-11 00:46:57 +0100

    hey i added things

---
+++ config/database.php
@@ -0,0 +1,24 @@
+<?php
+
+return  [
+
+   'driver'    => 'mysql',
+   
+   // Your DB driver
+    'host'      => 'localhost' , 
+     // Your DB host
+    'database'  => 'bayscope',
+     // Your DB name
+    'username'  => 'root',
+     // Your DB username
+    'password'  => '',
+     // Your DB password
+    'charset'   =>  'utf8'
+    ,
+    'collation' => 'utf8_unicode_ci',
+
+    'prefix'    =>  ''
+
+
+
+];
